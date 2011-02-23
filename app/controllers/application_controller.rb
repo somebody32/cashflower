a4b0bc1471c7c41e536270531e1f6581ab0e57e9
Ilya Zayats <somebody32@gmail.com>
2011-02-23 04:14:35 +0300

    added html5 boilerplate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :prepend_mobile_path
 
   protect_from_forgery
+
+  private
+
+  def prepend_mobile_path
+     if request.user_agent.to_s.downcase =~ Regexp.new(Cashflower::IOS_USER_AGENTS)
+       prepend_view_path(Rails.root + 'app' + 'views' + 'ios')
+     end
+  end
 end
--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -3,17 +3,24 @@ require 'spec_helper'
 describe StaticController do
 
   context "/index" do
-    it "should renders guest page for non-logged in users" do
+    it "should render guest page for non-logged in users" do
       get :index
       response.should render_template("guest")
     end
 
-    it "should renders nomal view for others" do
+    it "should render normal view for others" do
       user = Factory(:user)
       sign_in user
       get :index
 
       response.should render_template("index")
+    end
+
+    it "should render personal layout for iphone" do
+      request.env["HTTP_USER_AGENT"] = 'iphone'
+      get :index
+
+      ApplicationController.superclass.view_paths.dup.should include("app/views/ios/")
     end
   end
 
