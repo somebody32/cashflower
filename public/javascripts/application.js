23b362a28f2b41786875facc226bf9c2b4866454
Ilya Zayats <somebody32@gmail.com>
2011-02-24 14:46:36 +0300

    added new cashflow window

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,9 @@
+// Templates
+var income_history_link_src  = '{{#cashflows}}{{#cashflow}}<li class="arrow"><span class="income_history"><a id="{{id}}" href="#">${{value}}</a></span></li>{{/cashflow}}{{/cashflows}}';
+var outcome_history_link_src = '{{#cashflows}}{{#cashflow}}<<li class="arrow"><span class="outcome_history"><a id="{{id}}" href="#">${{value}}</a></span></li>{{/cashflow}}{{/cashflows}}';
 
+var income_history_link  = Handlebars.compile(income_history_link_src);
+var outcome_history_link = Handlebars.compile(outcome_history_link_src);
 
 $(function() {
 
