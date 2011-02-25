40cdcad7d3ad4f23863c4d80ce134fe766ab4060
Ilya Zayats <somebody32@gmail.com>
2011-02-25 05:15:17 +0300

    added cache manifest

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Cashflower::Application.routes.draw do
 
+  match "/iphone.manifest" => IPHONE_CACHE_MANIFEST
+
   resources :cashflows, :only => [:index, :create]
 
   devise_for :users, :controllers => { :registrations => "registrations" }
